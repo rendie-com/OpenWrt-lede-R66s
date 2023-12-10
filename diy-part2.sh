d77ef86e45b9910b6bc199e60858279f55ee40b3
rendie <121609433+rendie-com@users.noreply.github.com>
2023-12-10 14:16:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,6 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 
 # 移除重复软件包
 # rm -rf feeds/luci/themes/luci-theme-argon
-
 # 主题
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
