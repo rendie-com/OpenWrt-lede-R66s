4392f65a0bfa4c62d5a9ad9a55f6b225d04aa892
rendie <121609433+rendie-com@users.noreply.github.com>
2023-12-10 16:48:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,10 +7,10 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 #uci set network.wan.username='yougotthisfromyour@isp.su'
 #uci set network.wan.password='yourpassword'
 
-# 移除重复软件包
-# rm -rf feeds/luci/themes/luci-theme-argon
+# 移除重复软件包--以前的“luci-theme-argon”不好看
+rm -rf feeds/luci/themes/luci-theme-argon
 # 主题
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 调整 Docker 到 服务 菜单
 sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
