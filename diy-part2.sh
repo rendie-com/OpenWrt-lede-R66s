e07133ecfdaff73f4d18cdb0bf4e35553a55612d
rendie <121609433+rendie-com@users.noreply.github.com>
2024-08-20 09:10:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,7 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 #uci set network.wan.password='yourpassword'
 
 # 移除重复软件包--自带的“luci-theme-argon”不好看
-rm -rf feeds/luci/themes/luci-theme-argon
+# rm -rf feeds/luci/themes/luci-theme-argon
 # 主题
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
@@ -19,4 +19,4 @@ sed -i 's/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerma
 sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
 
 # TTYD 自动登录
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
